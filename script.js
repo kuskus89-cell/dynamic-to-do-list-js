4c71f6f1082bfea178b95dc82b64a1f3cf02515e
kuskus89-cell <sundusnoor89@gmail.com>
2025-12-12 14:52:03 +0000

    added a cjs file

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+//Select Elements
+    const addButton = document.getElementById('addButton'); 
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    //Define addTask function
+    function addTask (){
+        const taskText = taskInput.value.trim();
+
+        if (taskText === "" )
+            alert("Enter a task");
+        return;
+}
+    //Create <li>
+    const li = document.createElement("li");
+    li.textContent = taskText;
+    
+    //Create Remove Button
+    const removeBtn = document.createElement("button");
+    removeBtn.textContent = "Remove";
+    removeBtn.className = 'remove-btn';
+
+    //When clicked remove this task
+    removeBtn.onclick = function () {
+        tasklist.removeChild(li);
+    };
+
+    //attach button to li then li to list
+    li.appendChild(removeBtn);
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = "";
+
+
+    //add event listeners
+    addButton.addEventListener('click', addTask);
+
+    taskInput.addEventListener('keypress' , function(event) {
+        if(event.key === "Enter")
+        {
+            addTask();
+        }
+    });
+
+});
